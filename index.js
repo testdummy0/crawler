91cfc4f21d1d00787d1d83c3156a1d2f5d93cdea
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-12-05 00:58:20 +0530

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({ headless: false });
+    const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto('https://www.google.com');
     console.log("Title: " + await page.title());
